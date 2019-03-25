63f365b82a88c2dbd0c434bd806e976f52261428
YogeshMJadhav <43368583+YogeshMJadhav@users.noreply.github.com>
2019-03-25 16:39:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,45 +8,57 @@ import SEO from "../components/seo"
 
 const IndexPage = () => {
   return (
-      <div>
+     <div>
+              <form name=" contact" 
+                    method="post"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+              >
+                <input type="text" name="name" placeholder="name" />
+                <button>Save</button>
+              </form>  
+       </div>
+    
 
-      <form 
-          name="contact" 
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-       >
-      <div>
-        <div class="form-group col-md-4">
-          <label for="inputFirstName">First Name</label>
-          <input type="text" class="form-control" id="inputFirstName" placeholder="First Name"/>
-        </div>
+//       <form 
+//           name="contact" 
+//           method="post"
+//           data-netlify="true"
+//           data-netlify-honeypot="bot-field"
+//        >
+//       <div>
+//         <div class="form-group col-md-4">
+//           <label for="inputFirstName">First Name</label>
+//           <input type="text" class="form-control" id="inputFirstName" placeholder="First Name"/>
+//         </div>
 
-        <div class="form-group col-md-4" >
-          <label for="inputLastName">Last Name</label>
-          <input type="text" class="form-control" id="inputLastName" placeholder="Last Name"/>
-        </div>
+//         <div class="form-group col-md-4" >
+//           <label for="inputLastName">Last Name</label>
+//           <input type="text" class="form-control" id="inputLastName" placeholder="Last Name"/>
+//         </div>
         
-        <div class="form-group col-md-4" >
-          <label for="inputEmail4">Email</label>
-          <input type="text" class="form-control" id="inputEmail" placeholder="Email"/>
-        </div>
-      </div>
+//         <div class="form-group col-md-4" >
+//           <label for="inputEmail4">Email</label>
+//           <input type="text" class="form-control" id="inputEmail" placeholder="Email"/>
+//         </div>
+//       </div>
 
-      <div class="form-group col-md-4">
-        <label for="inputAddress" >Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-      </div>
+//       <div class="form-group col-md-4">
+//         <label for="inputAddress" >Address</label>
+//         <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+//       </div>
 
-      <div class="form-group col-md-4">
-        <label for="inputSubject">Subject</label>
-        <input type="text" class="form-control" id="inputSubject" placeholder="Subject"/>
-      </div>
+//       <div class="form-group col-md-4">
+//         <label for="inputSubject">Subject</label>
+//         <input type="text" class="form-control" id="inputSubject" placeholder="Subject"/>
+//       </div>
       
-      <button type="submit" class="btn btn-primary">submit</button>
-    </form>
+//       <button type="submit" class="btn btn-primary">submit</button>
+//     </form>
 
-    </div>
+    
+//    </div>
+//     </div>
     )
 }
 
