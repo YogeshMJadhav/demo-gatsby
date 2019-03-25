b94c6636374a8d4bbbc8d3db9797059131a0acdb
YogeshMJadhav <43368583+YogeshMJadhav@users.noreply.github.com>
2019-03-25 15:51:44 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,64 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   
   <div>
-          <form name=" contact" 
-                method="post"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-          >
-            <input type="text" name="First name" placeholder="name" />
-            <input type="text" name="Last Name" placeholder="Last Name" />
-            <input type="email" name="Email" placeholder="email" /> 
-            <input type="text" name="Address" placeholder="address" />
-            <input type="areaText" name="Subject" placeholder="subject" />
-            <button>Submit</button>
-          </form>  
+//           <form name=" contact" 
+//                 method="post"
+//                 data-netlify="true"
+//                 data-netlify-honeypot="bot-field"
+//           >
+//             <input type="text" name="First name" placeholder="name" />
+//             <input type="text" name="Last Name" placeholder="Last Name" />
+//             <input type="email" name="Email" placeholder="email" /> 
+//             <input type="text" name="Address" placeholder="address" />
+//             <input type="areaText" name="Subject" placeholder="subject" />
+//             <button>Submit</button>
+//           </form> 
+  <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
           
    </div>
 )
