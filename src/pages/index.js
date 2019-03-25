79c9897fa5e3c93126cbc9b102a47f8412e7750c
YogeshMJadhav <43368583+YogeshMJadhav@users.noreply.github.com>
2019-03-25 13:54:06 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,28 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  // <Layout>
+  //   <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+  //   <h1>Hi people</h1>
+  //   <p>Welcome to your new Gatsby site.</p>
+  //   <p>Now go build something great.</p>
+  //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //     <Image />
+  //   </div>
+  //   <Link to="/page-2/">Go to page 2</Link>
+  // </Layout>
+  <div>
+          <form name=" contact" 
+                method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+          >
+            <input type="text" name="name" placeholder="name" />
+            <button>Save</button>
+          </form>  
+          
+   </div>
 )
 
 export default IndexPage
+
