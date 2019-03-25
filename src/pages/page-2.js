68b87d261b3b4e1ab3b29452188e75def2cb4d4d
YogeshMJadhav <43368583+YogeshMJadhav@users.noreply.github.com>
2019-03-25 13:41:37 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,15 +2,31 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+const IndexPage = () => (
+  // <Layout>
+  //   <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+  //   <h1>Hi people</h1>
+  //   <p>Welcome to your new Gatsby site.</p>
+  //   <p>Now go build something great.</p>
+  //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //     <Image />
+  //   </div>
+  //   <Link to="/page-2/">Go to page 2</Link>
+  // </Layout>
+  <div>
+          <form name=" contact" method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+          >
+            <input type="text" name="name" placeholder="name" />
+            <input type="text" email="email" placeholder="email"  />
+            <button>Save</button>
+          </form>  
+          
+   </div>
 )
 
-export default SecondPage
+export default IndexPage
